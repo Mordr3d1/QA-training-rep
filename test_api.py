24e53c0c8a10e32635904c9da59d70f155c90b08
Mordr3d1 <96416884+Mordr3d1@users.noreply.github.com>
2023-09-18 23:10:45 +0400

    Api test final test function1

--- test_api.py
+++ test_api.py
@@ -13,7 +13,7 @@ def test_courses():
 def test_login_route():
 
     payload = {
-        "username":"joe",
+        "username":"joe1",
         "password":"12345678"
     }
     response = client.post("/login", json=payload)
@@ -25,7 +25,7 @@ def test_login_route():
 
 def test_registration_route():
     payload = {
-        "username": "joe1",
+        "username": "joe",
         "password": "12345678",
         "e-mail": "joe@doe.com"
 }
